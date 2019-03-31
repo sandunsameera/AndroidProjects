5b8b35ab0f59c3c83a13f7973003cbc035d15dd3
sandunsameera <sandunsameera25@gmail.com>
2019-03-31 10:08:47 +0530

    Adding question list

--- app/src/main/java/com/lolin/deemon_face/careforyou/RecentQuestionsFragment.java
+++ app/src/main/java/com/lolin/deemon_face/careforyou/RecentQuestionsFragment.java
@@ -7,16 +7,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class RecentQuestionsFragment extends Fragment {
 
     private static final String TAG = "Recent Questions";
+
+    public RecentQuestionsFragment(){
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate (R.layout.recent_questions_fragment,container,false);
+
+        String[] RecentQuestions = {"First Question",
+                "Second question",
+                "Third question"};
+
+        ListView listView = view.findViewById (R.id.Question_listView);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String> (
+                getActivity (),
+                android.R.layout.simple_list_item_1,
+                RecentQuestions
+
+        );
+
+        listView.setAdapter (listViewAdapter);
         return view;
     }
 }
